d60f4ad760f14bb137c49b3eb7c144209fa2f951
Shahid Khan <107818138+ShahidHKhan@users.noreply.github.com>
2025-09-27 17:03:53 -0400

    adding part 5, which is the intergration test (#32)
    * adding part 5, which is the intergration test
    
    - this SHOULD in theory have wired up the "full pipeline"
    
    - connecting UserComputeAPIImpl → InterComputeAPIImpl → InMemoryDataComputeAPI
    
    - then we give it an input
    
    * revamping step 5 so that gradle runs green
    
    the issue was that i didnt have dependencies and stuff, so the code didnt flow. assertions SHOULD match the desired output, i hope...
    
    * switching from (no-arg & setters) to constructor injecting

--- src/project/usercompute/UserRequest.java
+++ src/project/usercompute/UserRequest.java
@@ -1,9 +1,13 @@
 package project.usercompute;
 
 public class UserRequest {
+    private final byte[] bytes;
 
-	public UserRequest(byte[] bytes) {
-		// TODO Auto-generated constructor stub
-	}
+    public UserRequest(byte[] bytes) {
+        this.bytes = bytes;
+    }
 
+    public byte[] getBytes() {
+        return bytes;
+    }
 }
