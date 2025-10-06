30def2ab10961a638ce0be40e869fd2104ded8f5
Shahid Khan <107818138+ShahidHKhan@users.noreply.github.com>
2025-10-05 22:09:56 -0400

    Update DataComputeAPIFileImpl.java

--- src/project/datacompute/DataComputeAPIFileImpl.java
+++ src/project/datacompute/DataComputeAPIFileImpl.java
@@ -57,7 +57,7 @@ public class DataComputeAPIFileImpl implements DataComputeAPI {
                 writer.newLine();
             }
         } catch (Exception ignore) {
-            
+            return;
         }
     }
 
