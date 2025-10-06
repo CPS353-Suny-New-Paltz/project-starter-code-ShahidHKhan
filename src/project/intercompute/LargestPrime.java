5badf0a2d2a31fb695848a83ceb8794ce49cd86d
Shahid Khan <107818138+ShahidHKhan@users.noreply.github.com>
2025-10-05 21:59:25 -0400

    Update LargestPrime.java
    fixed the if statments to compute with checkstyle

--- src/project/intercompute/LargestPrime.java
+++ src/project/intercompute/LargestPrime.java
@@ -16,9 +16,15 @@ public class LargestPrime {
      //Checks whether a number is prime.
 
     private static boolean isPrime(int num) {
-        if (num < 2) return false;
-        if (num == 2) return true;
-        if (num % 2 == 0) return false;
+        if (num < 2) {
+              return false;
+          }
+        if (num == 2) {
+              return true;
+          }
+        if (num % 2 == 0) {
+              return false;
+          }
 
         for (int i = 3; i * i <= num; i += 2) {
             if (num % i == 0) {
