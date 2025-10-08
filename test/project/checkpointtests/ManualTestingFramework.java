454a8e36909101f834548fdc7569226ca848fa9e
Shahid Khan <107818138+ShahidHKhan@users.noreply.github.com>
2025-10-08 14:44:13 -0400

    Ckpt4.6 adding checkpoint4test + manualTestingFramwork (#36)
    * fixing 3.5
    
    * ckpt4
    
    * added new Test ( if input below 2)
    
    added a unit test in TestInterComputeAPI to verify how the computation behaves with edge-case inputs (e.g., numbers < 2). This ensures that the LargestPrime logic correctly returns "none" when no valid prime exists.
    
    * adding checkpoint4test + manualTestingFramwork
    
    * step6final
    
    * ensure output file is written as a single CSV line

---
+++ test/project/checkpointtests/ManualTestingFramework.java
@@ -0,0 +1,35 @@
+package project.checkpointtests;
+
+import project.datacompute.DataComputeAPI;
+import project.datacompute.DataComputeAPIFileImpl;
+import project.intercompute.InterComputeAPI;
+import project.intercompute.InterComputeAPIImpl;
+import project.usercompute.UserComputeAPI;
+import project.usercompute.UserComputeAPIImpl;
+
+
+public class ManualTestingFramework {
+    
+    public static final String INPUT = "manualTestInput.txt";
+    public static final String OUTPUT = "manualTestOutput.txt";
+
+    public static void main(String[] args) {
+        // TODO 1:
+        // Instantiate a real (ie, class definition lives in the src/ folder) implementation 
+        // of all 3 APIs
+        
+    	DataComputeAPI data = new DataComputeAPIFileImpl();
+    	InterComputeAPI inter = new InterComputeAPIImpl(data);
+    	UserComputeAPI user = new UserComputeAPIImpl(inter, data);
+    	
+        // TODO 2:
+        // Run a computation with an input file of <root project dir>/manualTestInput.txt
+        // and an output of <root project dir>/manualTestOutput.txt, with a delimiter of ',' 
+    	
+    	user.handle(INPUT, OUTPUT);
+    	
+        // Helpful hint: the working directory of this program is <root project dir>,
+        // so you can refer to the files just using the INPUT/OUTPUT constants. You do not 
+        // need to (and should not) actually create those files in your repo
+    }
+}
