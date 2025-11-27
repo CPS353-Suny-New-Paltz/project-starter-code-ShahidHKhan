200066ded2464b0fd04141e1f700b86f0e67cd1e
Shahid Khan <107818138+ShahidHKhan@users.noreply.github.com>
2025-11-27 12:56:43 -0500

    checkstyle edit

--- test/project/intercompute/TestFastInterComputeAPI.java
+++ test/project/intercompute/TestFastInterComputeAPI.java
@@ -1,7 +1,7 @@
 package project.intercompute;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestFastInterComputeAPI {
 
